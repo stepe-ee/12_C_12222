c755dbf4f53b102dc85cb93fe0d6433316e7d301
stepe-ee <steve.anggana@gmail.com>
2024-12-03 23:20:45 +0700

    12_C_12222_PAGI.js

--- 12_C_12222_PAGI.js
+++ 12_C_12222_PAGI.js
@@ -1,30 +1,47 @@
-console.log("PENDAFTARAN MAHASISWA BARU UNIVERSITAS CENDRAWASIH");
-console.log('_'.repeat(50));
+console.log("PENDAFTARAN MAHASISWA BARU UNIVERSITAS CENDRAWASIH")
+console.log('_'.repeat(50))
+
+function validateBirthDate(tanggal) {
+    const today = new Date()
+    const birthDate = new Date(tanggal)
+    const age = today.getFullYear() - birthDate.getFullYear()
+    const monthDifference = today.getMonth() - birthDate.getMonth()
+
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+        age--
+    }
+
+    if (isNaN(birthDate.getTime()) || age < 17) {
+        throw new Error("Student is too young")
+    }
+}
 
 function validateData(studentData) {
     return new Promise((resolve, reject) => {
-        const today = new Date();
-        const birthDate = new Date(studentData.tanggal);
-        const age = today.getFullYear() - birthDate.getFullYear();
+        try {
+            if (!studentData.nama || typeof studentData.nama !== "string") {
+                throw new Error("Invalid data")
+            }
 
-        if (!studentData.nama || typeof studentData.nama !== "string") {
-            reject(new Error("Invalid data"));
-        } else if (isNaN(birthDate.getTime()) || age < 17) {
-            reject(new Error("Student is too young"));
-        } else if (studentData.ktp.length !== 16 || isNaN(Number(studentData.ktp))) {
-            reject(new Error("Invalid KTP number"));
-        } else {
+            validateBirthDate(studentData.tanggal)
+
+            if (studentData.ktp.length !== 16 || isNaN(Number(studentData.ktp))) {
+                throw new Error("Invalid KTP number")
+            }
+
             resolve(`Student ${studentData.nama} has been successfully registered`);
+        } catch (error) {
+            reject(error)
         }
-    });
+    })
 }
 
 async function processData(studentData) {
     try {
-        const result = await validateData(studentData);
-        console.log(result);
+        const result = await validateData(studentData)
+        console.log(result)
     } catch (error) {
-        console.error(`An error occurred: ${error.message}`);
+        console.error(`An error occurred: ${error.message}`)
     }
 }
 
@@ -34,6 +51,6 @@ const students = [
     { nama: "Kevin", tanggal: "2000-12-30", ktp: "12345" },
     { nama: "Steve", tanggal: "2004-07-20", ktp: "2317122221234567" },
     { nama: "Kay", tanggal: "1995-08-15", ktp: "1122334455667788" },
-];
+]
 
-students.forEach((student) => processData(student));
+students.forEach((student) => processData(student))
